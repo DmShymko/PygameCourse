ffdafcf005bb436b06142094675a374d47a02c97
Dzmitry Shymko <dmitrij.shymko@librus.pl>
2025-11-19 22:09:20 +0100

    Ads new project - PygameCourse. Adss display surface.

---
+++ basic_pygame_tutorial/1_display_surface.py
@@ -0,0 +1,21 @@
+import pygame
+
+#Initialize pygame
+pygame.init()
+
+#Create display surface ant set its caption
+WINDOW_WIDTH = 600
+WINDOW_HEIGHT = 300
+display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+pygame.display.set_caption("Hello World")
+
+#The main game loop
+running = True
+while running:
+    #Loop through a list of evnt objects that have occurred
+    for event in pygame.event.get():
+        print(event)
+        if event.type == pygame.QUIT:
+            running = False
+#End game
+pygame.quit()
